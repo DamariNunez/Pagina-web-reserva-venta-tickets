b1080f669b8e8fd85a1327fc5f5af6da190af8b0
DamariNunez <damarisdaniela888@gmail.com>
2022-12-21 10:35:02 -0500

    Crear modelos de tablas

---
+++ app/Models/Held.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Held extends Model
+{
+    use HasFactory;
+}
