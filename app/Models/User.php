130498b946886b46c1bf6760ca604069fbdc2a79
DamariNunez <damarisdaniela888@gmail.com>
2022-12-21 11:40:30 -0500

    Registro y Login

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,11 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'username',
+        'lastname',
         'email',
         'password',
+        'phone',
     ];
 
     /**
