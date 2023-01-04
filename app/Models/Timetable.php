21dc13bb2c17ba41fbc8c6c15d145be549ad86db
DamariNunez <damarisdaniela888@gmail.com>
2023-01-04 16:30:20 -0500

    Implementación de Soft Deletes

--- app/Models/Timetable.php
+++ app/Models/Timetable.php
@@ -4,10 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Timetable extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
 
     protected $fillable = [
         'time',
